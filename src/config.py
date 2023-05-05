e17cbeec88c77744dee03139d058b3646ae4f2d3
firstVersion <kuroisiratama@yahoo.co.jp>
2023-05-05 13:36:10 +0900

    Added set method in ConfigClass

--- src/config.py
+++ src/config.py
@@ -13,8 +13,9 @@ class ConfigValue:
         self.pattern = pattern
         self.validate(self.default)
 
-    def validate(self, value):
+    def validate(self, value) -> bool:
         assert re.match(self.pattern, value) is not None, err_validation_failed.format(self.pattern)
+        return True
 
 
 class Config:
@@ -40,6 +41,17 @@ class Config:
                 separators=(",", ": "),
             )
 
+    def __update_config(self):
+        with open(f"{self.__config_path}/config.json", "w", encoding="utf-8") as f:
+            json.dump(
+                self.__values,
+                f,
+                ensure_ascii=False,
+                indent=4,
+                sort_keys=True,
+                separators=(",", ": "),
+            )
+
     def __load_config(self):
         result: dict = {}
         with open(f"{self.__config_path}/config.json", "r", encoding="utf-8") as f:
@@ -58,6 +70,12 @@ class Config:
     def get(self, key: str):
         assert key in self.__values.keys(), err_undefined_key
         return self.__values[key]
+
+    def set(self, key: str, value: str):
+        assert key in self.__values.keys(), err_undefined_key
+        if len([d for d in self.__default if d.key == key and d.validate(value)]) == 1:
+            self.__values[key] = value
+            self.__update_config()
 
     def show(self):
         return self.__values
